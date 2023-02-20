829cd3620f334889637fb4045c82533c8e66e059
siennalind <sienna.lebeaulind@gmail.com>
2023-02-20 14:31:31 +0000

    add Search page

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import SearchForm from "./SearchForm"
+
+function Search () {
+    return (
+        <>
+            <h2>Search</h2>
+            <SearchForm />
+        </>
+    )
+}
+
+export default Search
