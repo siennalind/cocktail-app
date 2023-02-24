799c7ec1fe599beb80f02914c0b1286885a22ecf
siennalind <sienna.lebeaulind@gmail.com>
2023-02-24 09:21:04 +0000

    change name of Recipe to RecipeItem to be more descriptive

---
+++ src/components/RecipeItem.js
@@ -0,0 +1,19 @@
+function RecipeItem () {
+    return (
+        <>
+            <h3>Name</h3>
+            <ul>
+                <li>Ingredient</li>
+                <li>Ingredient</li>
+                <li>Ingredient</li>
+            </ul>
+            <ol>
+                <li>Instruction</li>
+                <li>Instruction</li>
+                <li>Instruction</li>
+            </ol>
+        </>
+    )
+}
+
+export default Recipe
